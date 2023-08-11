81a57032dfb2a550fd0ff1f886ae6fac4342702d
imsadhappy <imsadhappy@gmail.com>
2023-08-11 15:53:25 +0300

    Version 0.1.4

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,22 +1,24 @@
 # app/controllers/application_controller.rb
 class ApplicationController < ActionController::Base
-  before_action :current_details
+  before_action :set_current_details
   before_action :authenticate
-  before_action :change_locale
-  around_action :set_locale
+  before_action :set_locale
+  around_action :use_locale
 
   private
 
-  def set_locale(&)
+  def use_locale(&)
     I18n.with_locale(helpers.current_locale, &)
   end
 
-  def change_locale
+  def set_locale
     redirect_to request.path if helpers.preferred_locale
   end
 
   def authenticate
     session_id = cookies.signed[:session_id]
+    return unless session_id
+
     session_record = Session.find_by_id(session_id)
     if session_record
       Current.session = session_record
@@ -25,7 +27,7 @@ class ApplicationController < ActionController::Base
     end
   end
 
-  def current_details
+  def set_current_details
     Current.user_agent = request.user_agent
     Current.ip_address = request.ip
   end
